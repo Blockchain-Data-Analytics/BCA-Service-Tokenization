3531e954116c93af615ea417f0bd7b1d283d91a6
Alexander Diemand <codieplusplus@apax.net>
2024-10-02 00:09:06 +0200

    audit log and new layout
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ bca-token-app/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<nav class="w3-top w3-bar w3-black">
+	<div class="w3-container w3-padding-16">
+	<a href="/">Home</a>
+	<a href="/admin">Administration</a>
+	<a href="/log">Audit log</a>
+	</div>
+</nav>
+
+
+<div class="w3-container w3-padding-32">
+<slot></slot>
+</div>
+
+
+<footer class="w3-center w3-black w3-panel w3-padding-32">
+	<p>Powered by <a href="https://github.com/Blockchain-Data-Analytics" title="BCA Blockchain Analytics" target="_blank" ><img src="/bca_logo.png" alt="BCA Blockchain Analytics" width=24 height=24 /></a> BCA</p>
+</footer>
